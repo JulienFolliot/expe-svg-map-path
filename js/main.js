7919253e63680a10219fdd2f7b70fb9e022285d5
Julien Folliot <julien.folliot@10-vins.com>
2021-11-28 11:04:39 +0100

    Add path from gpx

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ var map = new L.Map("map", {
   zoom: 11
 });
 map.addLayer(layer);
+
+
+var gpx = 'coueron_nantes.gpx'; // URL to your GPX file or the GPX itself
+new L.GPX(gpx, {
+  async: true,
+  marker_options: {
+    startIconUrl: 'location.svg',
+    endIconUrl: 'location.svg',
+    shadowUrl: 'location-shadow.svg'
+  }
+}).on('loaded', function(e) {
+  map.fitBounds(e.target.getBounds());
+}).addTo(map);
